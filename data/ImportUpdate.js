eee8d398e9c3fd4298cf05b7ce5aaf80f9eea46f
Elaina Da Catto <vipnoxhd123@gmail.com>
2023-07-13 11:37:57 +0700

    Update V2.0.2

--- data/ImportUpdate.js
+++ data/ImportUpdate.js
@@ -1,3 +1,12 @@
 import utils from "./_utils"
+let datapath = new URL(".", import.meta.url).href
+
+// 2.0.2
+if (!DataStore.has("Custom-rank")) {
+	DataStore.set("Custom-rank", true)
+}
+
 window.addEventListener("load", ()=> {
+    utils.addCss("","","",`${datapath}ImportUpdate.css`)
 })
+//
--- data/Update/2.0.2/index.js
+++
@@ -1,5 +0,0 @@
-import "./index.css"
-
-if (!DataStore.has("Custom-rank")) {
-	DataStore.set("Custom-rank", true)
-}
