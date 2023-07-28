df67b80abd21baeafdf804013ec9e6132a96558f
Elaina Da Catto <vipnoxhd123@gmail.com>
2023-07-28 16:49:31 +0700

    Update Update-message.js

--- data/Update-message.js
+++ data/Update-message.js
@@ -8,18 +8,27 @@ if (DataStore.get(`Update-${update}`)) {
 		// Add this ""+"\n"+
 
 		`ElainaV3 Update ${update}`+"\n"+
+		"- Add custom Clash Trophy and Border"+"\n"+
+		"- Fix Custom rank"+"\n"+
+		"- Delete Auto Ban/Pick plugins (Thanks controlado for let me using it in old version)"+"\n"+
+		"- Add \"hide tab\" option"+"\n"+
+		"- Separate custom icon options"+"\n"+
+		"- You still can using auto Ban/Pick plugins by download it from Author's Github"+"\n"+
+
 		""+"\n"+
-		""+"\n"+
-		""+"\n"+
-		""+"\n"+
-		""+"\n"+
+
+		/*
 		"This update already installed"+"\n"+
 		""+"\n"+
 		"You don't have to do anything"+"\n"
-		//"You must download update version from github to get newest function"+"\n"+
-		//"If not, your theme still can run probably without new one"+"\n"+
-		//""+"\n"+
-		//"Sorry for this inconvenience"+"\n"
+		/**/
+
+		//*
+		"You must download update version from github to get newest function"+"\n"+
+		"If not, your theme still can run probably without new one"+"\n"+
+		""+"\n"+
+		"Sorry for this inconvenience"+"\n"
+		/**/
 	)
 	DataStore.set(`Update-${update}`, false)
 }
