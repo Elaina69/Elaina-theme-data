cf3cbcda0452309d3d3a7fbd85006f73cd6cb330
Elaina Da Catto <vipnoxhd123@gmail.com>
2023-08-16 17:08:35 +0700

    Update Update-message.js

--- data/Update-message.js
+++ data/Update-message.js
@@ -1,35 +1,78 @@
 ///*
-import update from './configs/Version'
+import update from './configs/Version.js'
+import lang from "./configs/Language.js"
+
 if (!DataStore.has(`Update-${update}`)) {
 	DataStore.set(`Update-${update}`, true)
 }
 if (DataStore.get(`Update-${update}`)) {
-	window.alert(
-		// Add this ""+"\n"+
+	// window.alert(
 
-		`ElainaV3 Update ${update}`+"\n"+
-		"- Add Open assets and configs folder buttons"+"\n"+
-		"- Add Custom regalia banner"+"\n"+
-		"- Add Custom mastery point"+"\n"+
-		"- Separate custom avatar and custom hover card backdrop"+"\n"+
-		"- Custom icon no more change friend's icons"+"\n"+
-		"- Increase refresh time for custom rank"+"\n"+
+	// 	"This update already installed"+"\n"+
+	// 	"You don't have to do anything"+"\n"
 
-		""+"\n"+
+	// 	"You must download update version from github to get newest function"+"\n"+
+	// 	"If not, your theme still can run probably without new one"+"\n"+
+	// 	"Sorry for this inconvenience"+"\n"
 
-		/*
-		"This update already installed"+"\n"+
-		""+"\n"+
-		"You don't have to do anything"+"\n"
-		/**/
+	async function createLoaderMenu(root) {
+		const langCode = document.querySelector("html").lang;
+		const langMap = lang.langlist
+		const _t = lang[langMap[langCode] || "EN"];
+		const { Component, jsx, render } = await import('//esm.run/nano-jsx')
+		
+		class LoaderMenu extends Component {
+			render() {
+				return jsx/*html*/`
+					<div class="modal" style="position: absolute; inset: 0px; z-index: 8500;" id="Elaina-Update">
+						<lol-uikit-full-page-backdrop class="backdrop" style="display: flex; align-items: center; justify-content: center; position: absolute; inset: 0px;"></lol-uikit-full-page-backdrop>
+						<div class="dialog-confirm" style="display: flex; align-items: center; justify-content: center; position: absolute; inset: 0px;">
+							  <lol-uikit-dialog-frame class="dialog-frame" orientation="bottom" close-button="false">
+								<div class="dialog-content">
+									<lol-uikit-content-block class="app-controls-exit-dialog" type="dialog-small" style="width: 500px;">
+										<h5>Elaina_V3 - Update ${update}</h5>
+										<hr class="heading-spacer" />
+										<p class="Elaina-Update">- Add Open assets and configs folder buttons</p>
+										<p class="Elaina-Update">- Add Custom regalia banner</p>
+										<p class="Elaina-Update">- Add Custom mastery point</p>
+										<p class="Elaina-Update">- Separate custom avatar and custom hover card backdrop</p>
+										<p class="Elaina-Update">- Custom icon no more change friend's icons</p>
+										<p class="Elaina-Update">- Increase refresh time for custom rank</p>
+										<p class="Elaina-Update">- Update custom status</p>
+										<p class="Elaina-Update">- Update old LeagueLoader settings</p>
+										<p class="Elaina-Update">- New Update message tab</p>
+										<p class="Elaina-Update">- New pure theme/plugins settings page </p>
+										<p class="Elaina-Update"></p>
+										<p class="Elaina-Update"></p>
+	
+									</lol-uikit-content-block>
+								</div>
+								<lol-uikit-flat-button-group type="dialog-frame">
+									<lol-uikit-flat-button tabindex="1" class="button-decline" onClick=${() => {document.getElementById("Elaina-Update").hidden = true}}>${_t['l.close']}</lol-uikit-flat-button>
+								</lol-uikit-flat-button-group>
+							  </lol-uikit-dialog-frame>
+						</div>
+					</div>
+				`
+			}
+		}
+		render(jsx`<${LoaderMenu} />`, root)
+	}
+	
+	window.addEventListener("load", async ()=> {
+		const manager = () => document.getElementById('lol-uikit-layer-manager-wrapper')
+		const root    = document.createElement('div')
+		while (!manager()) await new Promise(r => setTimeout(r, 200))
+		await createLoaderMenu(root)
+		manager().prepend(root)
+		window.setInterval(()=>{
+			try {
+				let closeButton = document.querySelector("#Elaina-Update > div > lol-uikit-dialog-frame").shadowRoot.querySelector("div > div.lol-uikit-dialog-frame-close-button > lol-uikit-close-button")
+				closeButton.addEventListener("click", ()=> {document.getElementById("Elaina-Update").hidden = true})
+			}catch{}
+		})
+	})
 
-		//*
-		"You must download update version from github to get newest function"+"\n"+
-		"If not, your theme still can run probably without new one"+"\n"+
-		""+"\n"+
-		"Sorry for this inconvenience"+"\n"
-		/**/
-	)
 	DataStore.set(`Update-${update}`, false)
 }
 //*/
