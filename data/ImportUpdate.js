cc70d2557f47acc4fe4059048c25374c5d8757aa
Elaina Da Catto <vipnoxhd123@gmail.com>
2023-08-16 17:08:27 +0700

    move data to lastest version

--- data/ImportUpdate.js
+++ data/ImportUpdate.js
@@ -2,62 +2,11 @@ import utils from "./_utils"
 let datapath = new URL(".", import.meta.url).href
 
 // 2.0.2
-if (!DataStore.has("Custom-rank")) {
-	DataStore.set("Custom-rank", true)
-}
-if (!DataStore.has("Runes-BG")) {
-	DataStore.set("Runes-BG", true)
-}
-
 window.addEventListener("load", ()=> {
     utils.addCss("","","",`${datapath}ImportUpdate.css`)
 })
 
-// 2.0.3
-// if (!DataStore.has("pick-delay")) {
-// 	DataStore.set("pick-delay", 500)
-// }
-// if (!DataStore.has("ban-delay")) {
-// 	DataStore.set("ban-delay", 500)
-// }
-
 //2.0.4
-if (!DataStore.has("hide-overview")) {
-	DataStore.set("hide-overview", true)
-}
-if (!DataStore.has("hide-merch")) {
-	DataStore.set("hide-merch", true)
-}
-if (!DataStore.has("hide-patch-note")) {
-	DataStore.set("hide-patch-note", true)
-}
-if (!DataStore.has("hide-esport")) {
-	DataStore.set("hide-esport", true)
-}
-if (!DataStore.has("Custom-Border")) {
-	DataStore.set("Custom-Border", true)
-}
-if (!DataStore.has("Custom-RP-Icon")) {
-	DataStore.set("Custom-RP-Icon", true)
-}
-if (!DataStore.has("Custom-BE-Icon")) {
-	DataStore.set("Custom-BE-Icon", true)
-}
-if (!DataStore.has("Custom-Rank-Icon")) {
-	DataStore.set("Custom-Rank-Icon", true)
-}
-if (!DataStore.has("Custom-Emblem")) {
-	DataStore.set("Custom-Emblem", true)
-}
-if (!DataStore.has("Custom-Clash-banner")) {
-	DataStore.set("Custom-Clash-banner", true)
-}
-if (!DataStore.has("Custom-Ticker")) {
-	DataStore.set("Custom-Ticker", true)
-}
-if (!DataStore.has("Custom-Trophy")) {
-	DataStore.set("Custom-Trophy", true)
-}
 let updateV204 = async (node) => {
     let pagename, previous_page
     pagename = node.getAttribute("data-screen-name")
@@ -90,23 +39,3 @@ let updateV204 = async (node) => {
 window.addEventListener("load", ()=> {
 	utils.mutationObserverAddCallback(updateV204, ["screen-root"])
 })
-
-//2.0.5
-if (!DataStore.has("Custom-Regalia-Banner")) {
-	DataStore.set("Custom-Regalia-Banner", true)
-}
-if (!DataStore.has("Custom-Hover-card-backdrop")) {
-	DataStore.set("Custom-Hover-card-backdrop", true)
-}
-if (!DataStore.has("Debug-mode")) {
-	DataStore.set("Debug-mode", false)
-}
-if (!DataStore.has("")) {
-	DataStore.set("", false)
-}
-if (!DataStore.has("")) {
-	DataStore.set("", false)
-}
-if (!DataStore.has("")) {
-	DataStore.set("", false)
-}
