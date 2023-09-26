55773eaab40588b4b617c32d09663dcfa8201802
Elaina Da Catto <vipnoxhd123@gmail.com>
2023-09-26 16:29:28 +0700

    Update Custom-Profile.js

--- data/built-in_plugins/Custom-Profile.js
+++ data/built-in_plugins/Custom-Profile.js
@@ -8,42 +8,47 @@ if ("Custom-profile-hover") {
     let divisionOptions = ["I", "II", "III", "IV"];
 
     window.setInterval(async ()=>{
-        if (DataStore.get("Custom-challenge-crystal")) {
-            try {
-                let requestChallengeCrystal = {
-                    "lol": {
-                        "challengePoints"       : `${DataStore.get("Challenge-Points")}`,
-                        "challengeCrystalLevel" : `${tierOptions[DataStore.get("challengeCrystalLevel")]}`
-                    }
+        try {
+            if (document.querySelector("#lol-uikit-tooltip-root > div > div > div.hover-card.right.has-regalia.regalia-loaded")) {
+                if (DataStore.get("Custom-challenge-crystal")) {
+                    try {
+                        let requestChallengeCrystal = {
+                            "lol": {
+                                "challengePoints"       : `${DataStore.get("Challenge-Points")}`,
+                                "challengeCrystalLevel" : `${tierOptions[DataStore.get("challengeCrystalLevel")]}`
+                            }
+                        }
+                        let checkID = document.querySelector("#lol-uikit-tooltip-root > div > div > div.hover-card.right.has-regalia.regalia-loaded > div > div.hover-card-info-container > div.hover-card-identity > lol-regalia-hovercard-v2-element").getAttribute("summoner-id")
+                        let text = document.querySelector("#lol-uikit-tooltip-root > div > div > div.hover-card.right.has-regalia.regalia-loaded > div > div.hover-card-info-container > div.hover-card-info > div > div.hover-card-challenge-crystal").innerText
+                        let check = text.includes(`${DataStore.get("Challenge-Points")}`)
+                        if (!check && checkID == DataStore.get("Summoner-ID")) {
+                            await fetch("/lol-chat/v1/me", {
+                                method : "PUT",
+                                headers: {"content-type": "application/json"},
+                                body   : JSON.stringify(requestChallengeCrystal)
+                            })
+                        }
+                    }catch{}
                 }
-                let checkID = document.querySelector("#lol-uikit-tooltip-root > div > div > div.hover-card.right.has-regalia.regalia-loaded > div > div.hover-card-info-container > div.hover-card-identity > lol-regalia-hovercard-v2-element").getAttribute("summoner-id")
-                let text = document.querySelector("#lol-uikit-tooltip-root > div > div > div.hover-card.right.has-regalia.regalia-loaded > div > div.hover-card-info-container > div.hover-card-info > div > div.hover-card-challenge-crystal").innerText
-                let check = text.includes(`${DataStore.get("Challenge-Points")}`)
-                if (!check && checkID == DataStore.get("Summoner-ID")) {
-                    await fetch("/lol-chat/v1/me", {
-                        method : "PUT",
-                        headers: {"content-type": "application/json"},
-                        body   : JSON.stringify(requestChallengeCrystal)
-                    })
+                if (DataStore.get("Custom-mastery-score")) {
+                    try {document.querySelector("div.style-profile-emblem-wrapper > div.style-profile-emblem-header > div.style-profile-champion-mastery-score").innerText = `${DataStore.get("Mastery-Score")}`}catch{}
+                    try {document.querySelector("div.collections-routes > div > div.control-panel > div.collection-details > div.collection-totals > div.total-owned.total-count.ember-view").innerText = `${DataStore.get("Mastery-Score")}`} catch{}
+                    try {
+                        let text = document.querySelector("#hover-card-header > div.hover-card-header-left > span.hover-card-mastery-score").innerText
+                        let check = text.includes(`${DataStore.get("Mastery-Score")}`)
+                        let checkID = document.querySelector("#lol-uikit-tooltip-root > div > div > div.hover-card.right.has-regalia.regalia-loaded > div > div.hover-card-info-container > div.hover-card-identity > lol-regalia-hovercard-v2-element").getAttribute("summoner-id")
+                        if (!check && checkID == DataStore.get("Summoner-ID")) {
+                            await fetch("/lol-chat/v1/me", {
+                                method : "PUT",
+                                headers: {"content-type": "application/json"},
+                                body   : JSON.stringify({"lol":{"masteryScore":`${DataStore.get("Mastery-Score")}`}})
+                            })
+                        }
+                    }catch{}
                 }
-            }catch{}
+            }
         }
-        if (DataStore.get("Custom-mastery-score")) {
-            try {document.querySelector("div.style-profile-emblem-wrapper > div.style-profile-emblem-header > div.style-profile-champion-mastery-score").innerText = `${DataStore.get("Mastery-Score")}`}catch{}
-            try {document.querySelector("div.collections-routes > div > div.control-panel > div.collection-details > div.collection-totals > div.total-owned.total-count.ember-view").innerText = `${DataStore.get("Mastery-Score")}`} catch{}
-            try {
-                let text = document.querySelector("#hover-card-header > div.hover-card-header-left > span.hover-card-mastery-score").innerText
-                let check = text.includes(`${DataStore.get("Mastery-Score")}`)
-                let checkID = document.querySelector("#lol-uikit-tooltip-root > div > div > div.hover-card.right.has-regalia.regalia-loaded > div > div.hover-card-info-container > div.hover-card-identity > lol-regalia-hovercard-v2-element").getAttribute("summoner-id")
-                if (!check && checkID == DataStore.get("Summoner-ID")) {
-                    await fetch("/lol-chat/v1/me", {
-                        method : "PUT",
-                        headers: {"content-type": "application/json"},
-                        body   : JSON.stringify({"lol":{"masteryScore":`${DataStore.get("Mastery-Score")}`}})
-                    })
-                }
-            }catch{}
-        }
+        catch {}
     },500)
 
     window.setInterval(async ()=>{
