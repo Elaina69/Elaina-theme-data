7531495f77f06665e0798f805740abd00b75d47d
Elaina Da Catto <vipnoxhd123@gmail.com>
2023-11-05 02:18:20 +0700

    Add message for holiday

---
+++ data/configs/Holiday.js
@@ -0,0 +1,54 @@
+let config = {
+    "1/1": {
+        "Day"    : "1/1",
+        "Text"   : "Happy new year !!",
+        "Image"  : "83110343_p1.jpg",
+        "filters": "brightness(1.0)"
+    },
+    "9/3": {
+        "Day"    : "9/3",
+        "Text"   : "Okaimono~~~ Okaimono~~~ ",
+        "Image"  : "miku.jpg",
+        "filters": "brightness(1.0)"
+    },
+    "31/10": {
+        "Day"    : "31/10",
+        "Text"   : "I'm pretty sure I'm dressed as a maid right now (=^ ◡ ^=)",
+        "Image"  : "24.jpg",
+        "filters": "brightness(1.0)"
+    },
+    "1/11": {
+        "Day"    : "1/11",
+        "Text"   : "Happy NNN day !!",
+        "Image"  : "Image8.png",
+        "filters": "brightness(0.8)"
+    },
+    "1/12": {
+        "Day"    : "1/12",
+        "Text"   : "Goodbye, NNN and Welcoom, DDD !!",
+        "Image"  : "85133902_p0.png",
+        "filters": "brightness(0.9)"
+    },
+    "25/12": {
+        "Day"    : "25/12",
+        "Text"   : "Merry Christmas !!",
+        "Image"  : "67987805_p1.jpg",
+        "filters": "brightness(1.0)"
+    },
+
+    // "4/11": {
+    //     "Day"    : "4/11",
+    //     "Text"   : "Sth to test",
+    //     "Image"  : "67987805_p1.jpg",
+    //     "filters": "brightness(1.0)"
+    // },
+
+    "/": {
+        "Day"    : "",
+        "Text"   : "",
+        "Image"  : "",
+        "filters": ""
+    },
+}
+
+export default config
