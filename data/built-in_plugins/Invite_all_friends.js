149e6c6daf971b67605e531d7837f73a34d1b3ae
Elaina Da Catto <vipnoxhd123@gmail.com>
2023-11-06 23:38:04 +0700

    Update Invite_all_friends.js

--- data/built-in_plugins/Invite_all_friends.js
+++ data/built-in_plugins/Invite_all_friends.js
@@ -17,31 +17,72 @@ async function InviteAll () {
         error: ""
     })
 
-    for(let i = 0; i < friendslist.length; i++) {
+    for(let i = 0; i < friendslist.length ; i++) {
         let summonerID = friendslist[i]["summonerId"]
         let availability = friendslist[i]["availability"]
 
-        let invite = await fetch("/lol-lobby/v2/lobby/invitations",{
-            method: 'POST',
-            headers: {"content-type": "application/json"},
-            body: JSON.stringify([{"toSummonerId": summonerID}])
-        })
-        if (invite.status == 200 && availability != "offline" && availability != "dnd") {Invited++}
+        if (DataStore.get("frGroupName") == friendslist[i]["groupName"]) {
+            let invite = await fetch("/lol-lobby/v2/lobby/invitations",{
+                method: 'POST',
+                headers: {"content-type": "application/json"},
+                body: JSON.stringify([{"toSummonerId": summonerID}])
+            })
+            if (invite.status == 200 && availability != "offline" && availability != "dnd" && availability != "mobile") {Invited++}
+        }
     }
 
     Toast.success(`Invited ${Invited} friends`)
 }
 
-let addInviteAllButton = () => {
-	if (document.querySelector(".lobby-header-buttons-container") != null && document.getElementById("inviteAllButton") == null) {
+let addInviteAllButton = async () => {
+	if (document.querySelector(".lobby-header-buttons-container") != null) {
+        let LCUfetch = await fetch('/lol-chat/v1/friends')
+        let friendslist = await LCUfetch.json()
+        let origin = []
+
+        for(let i = 0; i < friendslist.length ; i++) {
+            origin.push(friendslist[i]["groupName"])
+        }
+        let groupList = [...new Set(origin)]
+
+        const mainDiv = document.createElement("div")
+        mainDiv.id = "inviteAllDiv"
+        mainDiv.style.display = "flex"
+
         let button = document.createElement("lol-uikit-flat-button")
-        button.id = "inviteAllButton"
         button.textContent = "Invite all"
-        button.style.marginRight = "10px"
         button.onclick = async () => {InviteAll()}
 
+        let div = document.createElement("div")
+        div.classList.add("Dropdown-div")
+
+        let dropdown = document.createElement("lol-uikit-framed-dropdown")
+        dropdown.classList.add("lol-settings-general-dropdown")
+        dropdown.style.marginRight = "10px"
+        dropdown.style.width = "198px"
+
+        div.append(dropdown)
+
+        for (let i = 0; i < groupList.length; i++) {
+            let opt = groupList[i]
+            let el = document.createElement("lol-uikit-dropdown-option")
+            el.setAttribute("slot", "lol-uikit-dropdown-option")
+            el.innerText = opt
+            el.onclick = () => {
+                DataStore.set("frGroupName", opt)
+            }
+            if (DataStore.get("frGroupName") == opt) {
+                el.setAttribute("selected", "true")
+            }
+            dropdown.appendChild(el)
+        }
+
+        mainDiv.append(div,button)
+
         let gameBar = document.querySelector(".lobby-header-buttons-container")
-            gameBar.insertBefore(button, gameBar.children[1])
+            if (!document.querySelector("#inviteAllDiv")) {
+                gameBar.insertBefore(mainDiv, gameBar.children[1])
+            }
 	}
 }
 
