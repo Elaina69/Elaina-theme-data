c7155509c4ecc180c6d66837595fb0abd8c986c5
Elaina Da Catto <vipnoxhd123@gmail.com>
2024-03-18 02:01:58 +0700

    Update CommandBar.js

--- data/built-in_plugins/CommandBar.js
+++ data/built-in_plugins/CommandBar.js
@@ -8,27 +8,28 @@ CommandBar.addAction({
     hidden: false,
     perform: () => {
         let a = DataStore.get("seconds1")
-        let b = DataStore.get("seconds2")
         let c = DataStore.get("minutes1")
-        let d = DataStore.get("minutes2")
         let e = DataStore.get("hours1")
-        let f = DataStore.get("hours2")
 
-        DataStore.set("seconds1",9)
-		DataStore.set("seconds2",5)
-		DataStore.set("minutes1",9)
-        DataStore.set("minutes2",5)
+        DataStore.set("seconds1",59)
+		DataStore.set("minutes1",59)
 		DataStore.set("hours1",0)
-		DataStore.set("hours2",0)
 
         window.setTimeout(()=>{
             DataStore.set("seconds1",a+1)
-            DataStore.set("seconds2",b)
             DataStore.set("minutes1",c)
-            DataStore.set("minutes2",d)
             DataStore.set("hours1",e)
-            DataStore.set("hours2",f)
         },1000)
+    }
+})
+CommandBar.addAction({
+    name: "Backup datastore",
+    legend: "",
+    tags: ["ElainaV3","backup","datastore"],
+    group: "ElainaV3",
+    hidden: false,
+    perform: () => {
+        writeBackupData()
     }
 })
 // CommandBar.addAction({
