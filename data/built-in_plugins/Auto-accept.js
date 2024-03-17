f13d2177d60ee71b047d240b6de22f45175644c8
Elaina Da Catto <vipnoxhd123@gmail.com>
2024-03-18 02:00:26 +0700

    Update Auto-accept.js

--- data/built-in_plugins/Auto-accept.js
+++ data/built-in_plugins/Auto-accept.js
@@ -22,56 +22,68 @@ function fetch_or_create_champselect_buttons_container() {
 	}
 	else {
 		const div = document.createElement("div")
-
 		div.className = "cs-buttons-container"
-		document.querySelector(".v2-footer-notifications.ember-view").append(div)
-		return div
-	}
-}
 
-if (DataStore.get("auto_accept_button")) {
-	let autoAcceptCallback = async message => {
-	utils.phase = JSON.parse(message["data"])[2]["data"]
-	if (utils.phase == "ReadyCheck" && DataStore.get("auto_accept") && !queue_accepted) {
-		await acceptMatchmaking(),
-		queue_accepted = true
-	}
-	else if (utils.phase != "ReadyCheck") {
-		queue_accepted = false
-	}
-}
+		let nor = document.querySelector(".v2-footer-notifications.ember-view")
+		let tft = document.querySelector(".parties-footer-notifications.ember-view")
 
-let acceptMatchmaking = async () => {
-	if (player_declined) return;
-	await fetch('/lol-matchmaking/v1/ready-check/accept', { method: 'POST' })
-}
-
-let autoAcceptMutationObserver = (mutations) => {
-	if (document.querySelector(".v2-footer-notifications.ember-view") != null && document.getElementById("autoAcceptQueueButton") == null) {
-		const langCode= document.querySelector("html").lang
-		const langMap = lang.langlist
-		let newOption = document.createElement("lol-uikit-radio-input-option")
-		let container = fetch_or_create_champselect_buttons_container()
-		let Option2   = document.createElement("div")
-	
-		newOption.setAttribute("id", "autoAcceptQueueButton")
-		newOption.setAttribute("onclick", "window.autoAcceptQueueButton()")
-
-		Option2.classList.add("auto-accept-button-text")
-		Option2.innerHTML = lang[langMap[langCode] || "EN"]["auto_accept"]
-		if (DataStore.get("auto_accept")){
-			newOption.setAttribute("selected", "")
+		if (nor) {
+			nor.append(div)
+			return div
 		}
-		
-		container.append(newOption)
-		newOption.append(Option2)
+		else { 
+			tft.append(div)
+			return div
+		}
+	}	
+}
+
+
+	let autoAcceptCallback = async message => {
+		if (DataStore.get("auto_accept_button")) {
+			utils.phase = JSON.parse(message["data"])[2]["data"]
+			if (utils.phase == "ReadyCheck" && DataStore.get("auto_accept") && !queue_accepted) {
+				await acceptMatchmaking(),
+				queue_accepted = true
+			}
+			else if (utils.phase != "ReadyCheck") {
+				queue_accepted = false
+			}
+		}
 	}
-}
 
-window.autoAcceptQueueButton = autoAcceptQueueButton
+	let acceptMatchmaking = async () => {
+		if (player_declined) return;
+		await fetch('/lol-matchmaking/v1/ready-check/accept', { method: 'POST' })
+	}
 
-window.addEventListener('load', () => {
-	utils.subscribe_endpoint('/lol-gameflow/v1/gameflow-phase', autoAcceptCallback)
-	utils.routineAddCallback(autoAcceptMutationObserver, ["v2-footer-notifications.ember-view"])
-})
-}
+	let autoAcceptMutationObserver = () => {
+		if (document.querySelector(".v2-footer-notifications.ember-view") != null && document.getElementById("autoAcceptQueueButton") == null) {
+			const langCode= document.querySelector("html").lang
+			const langMap = lang.langlist
+			let newOption = document.createElement("lol-uikit-radio-input-option")
+			let container = fetch_or_create_champselect_buttons_container()
+			let Option2   = document.createElement("div")
+		
+			newOption.setAttribute("id", "autoAcceptQueueButton")
+			newOption.setAttribute("onclick", "window.autoAcceptQueueButton()")
+
+			Option2.classList.add("auto-accept-button-text")
+			Option2.innerHTML = lang[langMap[langCode] || "EN"]["auto_accept"]
+			if (DataStore.get("auto_accept")){
+				newOption.setAttribute("selected", "")
+			}
+			
+			if (DataStore.get("auto_accept_button")) {
+				container.append(newOption)
+				newOption.append(Option2)
+			}
+		}
+	}
+
+	window.autoAcceptQueueButton = autoAcceptQueueButton
+
+	window.addEventListener('load', () => {
+		utils.subscribe_endpoint('/lol-gameflow/v1/gameflow-phase', autoAcceptCallback)
+		utils.routineAddCallback(autoAcceptMutationObserver, ["v2-footer-notifications.ember-view"])
+	})
