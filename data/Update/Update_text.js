5d8413e8b908da11328f05c2bc36de7f433b0dfb
Elaina Da Catto <vipnoxhd123@gmail.com>
2024-03-30 18:25:42 +0700

    Update Update_text.js

--- data/Update/Update_text.js
+++ data/Update/Update_text.js
@@ -1,8 +1,4 @@
 export default [
-    "Refactoring Elaina theme's code",
-    "Fix all bug from v2.2.4",
-    "Now wallpaper/audio list only reload after you reload/restart client",
-    "Replace nsfw image link",
-    "Replace old language system",
-    "Last update before Vanguard for sure 😭 (again)",
+    "Fix custom animate gamemode icon (I forgot this actually))",
+    "Add some comment things"
 ]
