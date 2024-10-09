8727046cd3d75e01f8f4e758552e934634d60d18
Elaina Da Catto <vipnoxhd123@gmail.com>
2024-10-09 16:53:13 +0700

    Update v4.1.0

--- src/update/updateText.js
+++ src/update/updateText.js
@@ -1,9 +1,10 @@
 export default [
-    "Fix blue essence's Css",
-    "Add new banners",
-    "Fix datastore bug",
-    "Fix Filters bug",
-    "Change settings stuff",
-    "Change Kofi to Paypal link",
+    "Theme now CAN NOT auto get wallpaper/audio/banner/font list",
+    "Now you have to manually add wallpaper/audio/banner/font into theme inside settings (this should work for Mac version too) until new Pengu file system comeout",
+    "Now wallpaper/audio buttons only show when you hover those area",
+    "Add custom nickname color",
+    "Replace default wallpaper/audio with new one",
+    "Default audio volume now is 15%",
+    "Update utils.js",
     "",
 ]
