e241a59e4115fceccae4b229022eb6f27308bfbd
Elaina Da Catto <vipnoxhd123@gmail.com>
2025-01-26 02:37:58 +0700

    Change: Move settings to local

--- src/config/images.js
+++ src/config/images.js
@@ -8,17 +8,6 @@ export default [
     `${datapath}assets/champs/amiyi.png`,
     `${datapath}assets/champs/amiyi_preview.png`,
     `${datapath}assets/champs/amiyi_thumbnail.png`,
-    `${icon}/about-us/balaclava.png`,
-    `${icon}/about-us/dmitryfisk.png`,
-    `${icon}/about-us/elainadacatto.png`,
-    `${icon}/about-us/flirip.png`,
-    `${icon}/about-us/legnatbird.png`,
-    `${icon}/about-us/soulmare.png`,
-    `${icon}/about-us/unproductive.webp`,
     `${icon}/plugins-icons/elainacb.png`,
     `${icon}/plugins-icons/reroll_button.png`,
-    `${icon}/logo.png`,
-    `${icon}/momo.svg`,
-    `${icon}/ko-fi.webp`,
-    `${icon}/paypal.png`
 ]
